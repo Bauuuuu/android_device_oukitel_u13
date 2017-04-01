87e3a8c9a0244778327a79330167f832171db0bf
Bauuuuu <abansode@outlook.com>
2017-04-01 19:41:53 +0200

    add debugging props+more
    Change-Id: I39d51ae3ab555d5e48874304565ebeb6b8535461

--- device.mk
+++ device.mk
@@ -190,3 +190,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=MT6753 \
     ro.telephony.ril.config=fakeiccid \
     ro.com.android.mobiledata=false
+
+#Additional Default properties
+DDITIONAL_DEFAULT_PROPERTIES += \
+	ro.secure=0 \
+	ro.allow.mock.location=1 \
+	ro.debuggable=1 \
+	ro.adb.secure=0 \
+	persist.service.acm.enable=0 \
+    ro.oem_unlock_supported=1 \
+	ro.mount.fs=EXT4 \
+	camera.disable_zsl_mode=1
